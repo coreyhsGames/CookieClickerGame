a55dbb867b679b515d33383355b5515f608e86bc
coreyhsGames <83317368+coreyhsGames@users.noreply.github.com>
2025-08-04 13:00:15 +1200

    added main.js comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Variables
 let cookieButton = document.getElementById("cookieButton")
 let cookieAmount = document.getElementById("cookieAmount")
 let grandmaUpgrade = document.getElementById("grandmaUpgrade")
@@ -7,15 +8,18 @@ let cookies = 0
 let grandmas = 0
 let grandmaCost = 100
 
+// Used to update the text to display the number of cookies
 function updateCookieAmount() {
     cookieAmount.innerText = "You have " + cookies + " cookies!"
 }
 
+// Used when the cookie is clicked
 function clickCookie() {
     cookies++
     updateCookieAmount()
 }
 
+// Handles the purchasing of grandmas
 function purchaseGrandma() {
     if (cookies >= grandmaCost) {
         cookies -= grandmaCost
@@ -28,6 +32,7 @@ function purchaseGrandma() {
     }
 }
 
+// Adds cookies every second using the number of grandmas purchased
 setInterval(function() {
     cookies += grandmas
     updateCookieAmount()
